e8e65eb6c7dc53c6d1a4ff2dc3e7c99f9cb1fc46
Vladislava <stdemidovez@gmail.com>
2024-10-21 02:15:02 +0300

    feat: handle click on the burger menu

--- js/index.js
+++ js/index.js
@@ -5,3 +5,13 @@ import renderProjects from './renderProjects.js';
 
 renderProjects(projects);
 renderNavbarMenu(navbarMenuItems)
+document.addEventListener('DOMContentLoaded', function () {
+  const burger = document.querySelector('.hamburger');
+  const menuWrapper = document.querySelector('.navbar-menu-wrapper');
+
+  burger.addEventListener('click', function () {
+    menuWrapper.classList.toggle('open');
+    burger.classList.toggle('open');
+    burger.setAttribute('aria-expanded', menuWrapper.classList.contains('open') ? 'true' : 'false');
+  });
+});
